9cea83dea82b91c5cf637fa40e9139202b0e57f8
Millie Schmidt <65649134+MillieSchmidt@users.noreply.github.com>
2021-04-27 12:19:29 +0300

    Add files via upload

--- moles.js
+++ moles.js
@@ -1,3 +1,5 @@
+console.log("This game was created as the final project of frontendmasters.com bootcamp, and was made for web. I made some modifications for mobile, but you should still play it on your PC. it's much more enjoyable!");
+
 const moleArray = document.querySelectorAll('.mole');
 const progressWorm = document.querySelector('.progress-worm');
 let points = 0;
@@ -10,6 +12,10 @@ function goneToHungry (random) {
 }
 
 function hungryToSad (random) {
+    if (moleArray[random].classList.contains('fed')) {
+        fedToLeaving(random);
+    }
+    
     moleArray[random].classList.remove('hungry');
     moleArray[random].classList.add('sad');
 }
@@ -29,7 +35,11 @@ function kingToHungry (random) {
     moleArray[random].classList.add('king-hungry');
 }
 
-function kingToSad (random) {    
+function kingToSad (random) {
+    if (moleArray[random].classList.contains('king-fed')) {
+        kingToLeaving(random);
+    }
+
     moleArray[random].classList.remove('king-hungry');
     moleArray[random].classList.add('king-sad');
 }
@@ -47,6 +57,11 @@ function kingToGone (random) {
 // functions called by function feedAMole
 
 function hungryToFed (random) {
+    if (moleArray[random].classList.contains('sad')) {
+        moleArray[random].classList.remove('sad');
+    }
+    
+    console.log("hungryToFed called");
     moleArray[random].classList.remove('hungry');
     moleArray[random].classList.add('fed');
     points++;
@@ -85,7 +100,58 @@ function kingFedtoLeaving (random) {
 // beginning of newInterval
 
 let newInterval = setInterval(function () {
-    let random = Math.floor(Math.random() * 10);
+    let random = Math.floor(Math.random() * 8);
+    console.log(random);
+    
+    if ( Math.floor(Math.random() * 8) > 1 ) {
+
+    setTimeout(function () {
+        
+        goneToHungry(random);
+        
+        setTimeout(function () {
+            
+            hungryToSad(random);
+
+            setTimeout(function () {
+                
+                sadToLeaving(random);
+
+                setTimeout(function () {
+                    
+                    leavingToGone(random);
+                }, 500)
+
+            }, 500)
+
+        }, 1500)
+        
+    }, 100)
+
+    } else {
+        setTimeout(function () {
+            
+            kingToHungry(random);
+            
+            setTimeout(function () {
+                
+                kingToSad(random);
+    
+                setTimeout(function () {
+                    
+                    kingToLeaving(random);
+    
+                    setTimeout(function () {
+                        
+                        kingToGone(random);
+                    }, 500)
+    
+                }, 500)
+    
+            }, 1500)
+            
+        }, 100)
+    }
 
     function feedAMole() {
         if (moleArray[random].classList.contains('hungry')) {
@@ -122,57 +188,7 @@ let newInterval = setInterval(function () {
     }
 
     moleArray[random].addEventListener('click', feedAMole);
-
-    if ( Math.floor(Math.random() * 10) > 1 ) {
-
-    setTimeout(function () {
-        
-        goneToHungry(random);
-        
-        setTimeout(function () {
             
-            hungryToSad(random);
-
-            setTimeout(function () {
-                
-                sadToLeaving(random);
-
-                setTimeout(function () {
-                    
-                    leavingToGone(random);
-                }, 500)
-
-            }, 500)
-
-        }, 2000)
-        
-    }, Date.now())
-
-    } else {
-        setTimeout(function () {
-            
-            kingToHungry(random);
-            
-            setTimeout(function () {
-                
-                kingToSad(random);
-    
-                setTimeout(function () {
-                    
-                    kingToLeaving(random);
-    
-                    setTimeout(function () {
-                        
-                        kingToGone(random);
-                    }, 500)
-    
-                }, 500)
-    
-            }, 2000)
-            
-        }, Date.now())
-    }
-            
-}, 7000) //end of interval newInterval
+}, 4000) //end of interval newInterval
 
 //clearInterval(newInterval);
